ba54cb9be486e3754cdb062a49cd25331f354a1d
Instrumentality Foundation <instrumentality-foundation@outlook.com>
2020-06-25 19:46:53 +0300

    OFFTOPIC
    Added script to setup git when cloning this repo

---
+++ instrumentality.sh
@@ -0,0 +1,6 @@
+git config user.name "Instrumentality Foundation"
+git config user.email "instrumentality-foundation@outlook.com"
+git config user.signingKey C0CC9D9F2074E1F25AB2D0160AE064B04616274D
+git config core.sshCommand "ssh -i ~/.ssh/id_ed25519"
+git config remote.lab.url "git@gitlab.com:instrumentality-foundation/instrumentality-landing.git"
+git config remote.lab.fetch "+refs/heads/*:refs/remotes/origin/*"
