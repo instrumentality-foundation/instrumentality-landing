cb554898bfc64a9ccddf001032305e39f674429e
Instrumentality Foundation <instrumentality-foundation@outlook.com>
2020-06-26 14:27:13 +0300

    Fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,23 @@
 <template>
   <!-- Main component wrapper -->
-  <div class="navigation-component" uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-
+  <div
+    class="navigation-component"
+    uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky"
+  >
     <!-- Real navbar container -->
     <nav uk-navbar class="uk-navbar-container">
-
       <!-- This part will be placed on the left side -->
-      <div class="uk-navbar-left"><div>
-        <a href="#" class="uk-navbar-item uk-logo">
-          <img src="@/assets/Logo/Instrumentality-Logo.svg" alt="The Instrumentality Project Logo" />
-          <span class="uk-margin-left">Instrumentality</span>
-        </a>
-      </div></div>
+      <div class="uk-navbar-left">
+        <div>
+          <a href="#" class="uk-navbar-item uk-logo">
+            <img
+              src="@/assets/Logo/Instrumentality-Logo.svg"
+              alt="The Instrumentality Project Logo"
+            />
+            <span class="uk-margin-left">Instrumentality</span>
+          </a>
+        </div>
+      </div>
 
       <div class="uk-navbar-center">
         <ul class="uk-navbar-nav">
@@ -38,18 +44,22 @@
               <a href="">Signup</a>
             </li>
             <li id="burger-menu">
-              <button class="uk-button uk-button-default uk-button-large" type="button" uk-toggle="target: #offcanvas-nav-primary"><font-awesome-icon :icon="['fa', 'bars']" /></button>
+              <button
+                class="uk-button uk-button-default uk-button-large"
+                type="button"
+                uk-toggle="target: #offcanvas-nav-primary"
+              >
+                <font-awesome-icon :icon="['fa', 'bars']" />
+              </button>
             </li>
           </ul>
         </div>
       </div>
-
     </nav>
 
     <!-- This part describes the off-canvas navbar that appears only on mobile devices -->
     <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true; flip: true;">
       <div class="uk-offcanvas-bar uk-flex uk-flex-comlumn uk-flex-center">
-        
         <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
           <li>
             <a href="">Team updates</a>
@@ -64,23 +74,20 @@
             <hr />
           </li>
           <li>
-              <a href="">Login</a>
-            </li>
-            <li>
-              <a href="">Signup</a>
-            </li>
+            <a href="">Login</a>
+          </li>
+          <li>
+            <a href="">Signup</a>
+          </li>
         </ul>
-
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
 };
 </script>
 
@@ -91,34 +98,39 @@ export default {
   background-color: white !important;
 }
 
-.uk-navbar-nav li a, .uk-offcanvas-bar .uk-nav li a {
-    color: @text-color;
-    font-size: 1em;
-    font-family: 'Noto Sans', sans-serif;
-    text-decoration: none;
-    position: relative;
+.uk-navbar-nav li a,
+.uk-offcanvas-bar .uk-nav li a {
+  color: @text-color;
+  font-size: 1em;
+  font-family: "Noto Sans", sans-serif;
+  text-decoration: none;
+  position: relative;
 }
 
-.uk-navbar-nav li a::before, .uk-offcanvas-bar .uk-nav li a::before { // This is adding a nice underline effect
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: @brand-color;
-    visibility: hidden;
-    transform: scaleX(0);
-    transition: all 0.3s ease-in-out 0s;
+.uk-navbar-nav li a::before,
+.uk-offcanvas-bar .uk-nav li a::before {
+  // This is adding a nice underline effect
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: @brand-color;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
 }
 
-.uk-navbar-nav li a:hover::before, .uk-offcanvas-bar .uk-nav li a:hover::before {
-    visibility: visible;
-    transform: scaleX(1);
+.uk-navbar-nav li a:hover::before,
+.uk-offcanvas-bar .uk-nav li a:hover::before {
+  visibility: visible;
+  transform: scaleX(1);
 }
 
-.uk-navbar-nav li a:hover, .uk-offcanvas-bar .uk-nav li a:hover {
-    color: @brand-color;
+.uk-navbar-nav li a:hover,
+.uk-offcanvas-bar .uk-nav li a:hover {
+  color: @brand-color;
 }
 
 .uk-logo {
@@ -139,32 +151,34 @@ export default {
 }
 
 .uk-offcanvas-bar .uk-nav li hr {
-    border-color: @brand-color-sister;
-    margin-top: 1.5em;  .uk-card {
-    box-shadow: 0 5px 15px rgba(0,0,0,.08);
+  border-color: @brand-color-sister;
+  margin-top: 1.5em;
+  .uk-card {
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.08);
   }
 }
 
-  .uk-navbar-container {
-    background-color: rgba(255, 255, 255, 0.7) !important;
-    backdrop-filter: blur(1em) saturate(150%);
-  }
+.uk-navbar-container {
+  background-color: rgba(255, 255, 255, 0.7) !important;
+  backdrop-filter: blur(1em) saturate(150%);
+}
 
-  .uk-offcanvas-bar {
-    background-color: white;
-  }
+.uk-offcanvas-bar {
+  background-color: white;
+}
 
 // As display size goes up...
 
 // What happens with small and medium devices -- Phones & Tablets
 @media only screen and (max-width: 960px) {
-  .uk-navbar-right li, .uk-navbar-center {
+  .uk-navbar-right li,
+  .uk-navbar-center {
     display: none;
   }
 
   .uk-offcanvas-bar .uk-nav li a {
     font-size: 1.3em;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: "Noto Sans", sans-serif;
   }
 
   #burger-menu {
@@ -175,14 +189,14 @@ export default {
   }
 
   #burger-menu button {
-    background: rgba(255, 255, 255, 0);  
+    background: rgba(255, 255, 255, 0);
   }
 }
 
 // What happens with medium sized devices -- Tablets
 @media only screen and (min-width: 640px) {
   .uk-logo {
-    font-size: 1.50em;
+    font-size: 1.5em;
   }
 }
 
